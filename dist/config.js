6e74d694bbd2ba48067b0e59a668544f4e9ab018
Lars Erik Finholt <lars.erik.finholt@gmail.com>
2013-07-02 14:41:43 +0200

    included dist folder

---
+++ dist/config.js
@@ -0,0 +1,70 @@
+var path = require('path');
+var config = {
+  "watch": {
+    "sourceDir": [
+      "assets"
+    ],
+    "compiledDir": [
+      "public"
+    ],
+    "javascriptDir": [
+      "App"
+    ],
+    "exclude": [],
+    "throttle": 0,
+    "excludeRegex": {},
+    "compiledJavascriptDir": [
+      "public",
+      "App"
+    ]
+  },
+  "liveReload": {
+    "enabled": false
+  },
+  "server": {
+    "defaultServer": {
+      "enabled": false,
+      "onePager": false
+    },
+    "path": [
+      "server.js"
+    ],
+    "port": 3000,
+    "base": "",
+    "views": {
+      "compileWith": "ejs",
+      "extension": "html",
+      "path": [
+        "views"
+      ],
+      "html": true
+    }
+  }
+}
+
+var resolvePath = function (pathPieces) {
+  var returnPath = __dirname;
+  pathPieces.forEach(function(piece) {
+    returnPath = path.join(returnPath, piece);
+  })
+  return returnPath;
+}
+
+if (config.watch) {
+  config.watch.compiledDir = resolvePath(config.watch.compiledDir);
+  config.watch.sourceDir = resolvePath(config.watch.sourceDir);
+  config.watch.javascriptDir = resolvePath(config.watch.javascriptDir)
+  config.watch.compiledJavascriptDir = resolvePath(config.watch.compiledJavascriptDir);
+}
+
+if (config.server) {
+  if (config.server.path) {
+    config.server.path = resolvePath(config.server.path);
+  }
+
+  if (config.server.views && config.server.views.path) {
+    config.server.views.path = resolvePath(config.server.views.path);
+  }
+}
+
+module.exports = config;
